d463c75ad11812730dcc960c0ab63241ff85bedd
sekab <amrtaher1995@gmail.com>
2019-02-23 12:55:43 +0200

    changed the results to get the data as needed and reduce code in other components

--- src/app/articles/article/article.service.ts
+++ src/app/articles/article/article.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map, tap, finalize } from 'rxjs/operators';
+import { GlobalService } from 'src/app/global.service';
 
 export interface Post {
   header: string ;
@@ -9,7 +11,7 @@ export interface Post {
   tag: string[] ;
   icon: string ;
   time: string ;
-  content: any[];
+  content?: any[];
   id?: string;
   post?: string;
 }
@@ -28,25 +30,42 @@ export class ArticleService {
 
   currentPost: Observable<Post>;
   constructor(private http: HttpClient,
+    private globalService: GlobalService,
     private db: AngularFirestore) {
 
    }
-   public fetchPosts() {
-    return this.db.collection('Posts').get().toPromise();
+   public fetchPosts(): Observable<Post[]> {
+    return this.globalService.loaderSubscription(this.db.collection('Posts').get().pipe(
+      map(snap => {
+      const result = [] as Post[];
+      snap.forEach(s => result.push(s.data() as Post));
+      return result;
+    })));
    }
-   public getPost(header) {
-     return  this.db.firestore.collection('Posts').where('header' , '==' , header).get();
+   public getPost(header): Promise<Post> {
+     return this.globalService.loaderPromises(
+       this.db.firestore.collection('Posts').where('header' , '==' , header).get()
+     .then(res => {
+       let postResult = {} as Post;
+       postResult = res.docs[0].data() as Post;
+       postResult.id = res.docs[0].id;
+       return postResult;
+     }));
    }
    public addPost(post: Post) {
-     return this.db.firestore.collection('Posts').add(post);
+
+    return this.globalService.loaderPromises(this.db.firestore.collection('Posts').add(post));
    }
    public getPostById(id) {
-     return this.db.firestore.collection('Posts').doc(id).get();
+    return this.globalService.loaderPromises(this.db.firestore.collection
+      ('Posts').doc(id).get());
    }
    public updatePost(post: Post , id: string) {
-     return this.db.firestore.collection('Posts').doc(id).set(post);
+     return this.globalService.loaderPromises
+     (this.db.firestore.collection('Posts').doc(id).set(post));
    }
    public upVote(id: string , value: number) {
-     return this.db.firestore.collection('Posts').doc(id).update({votes : value});
+     return this.globalService.loaderPromises
+     (this.db.firestore.collection('Posts').doc(id).update({votes : value}));
    }
 }
